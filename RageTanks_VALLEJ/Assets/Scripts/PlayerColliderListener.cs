f130434fcb6e46d97d26412eade916958a977c03
vallejestudiante <vallejestudiante@gmail.com>
2019-10-29 19:44:12 +0100

    PT2.3.2 Terminada

---
+++ RageTanks_VALLEJ/Assets/Scripts/PlayerColliderListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using
+System.Collections;
+public class PlayerColliderListener : MonoBehaviour
+{
+    public PlayerStateListener targetStateListener = null;
+    void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        switch (collidedObject.tag)
+        {
+            case "Platform":
+                // Quan el Player cau en una plataforma, canviar l'estat.
+                Debug.Log(collidedObject.tag);
+                targetStateListener.onStateChange(
+                PlayerStateController.playerStates.landing);
+                break;
+        }
+    }
+}
